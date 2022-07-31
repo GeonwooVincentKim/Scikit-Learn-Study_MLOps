967e5834995972e7cc7aab602609f704384a49f5
GeonwooVincentKim <kdsnop@gmail.com>
2022-07-31 21:37:02 +0900

    [Update] - Updated files

--- example/test.py
+++ example/test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # import os
 
 # from random import random, randint
@@ -10,3 +11,6 @@ mlflow.start_run()
 mlflow.log_param("my", "param")
 mlflow.log_metric("score", 100)
 mlflow.end_run()
+=======
+print("Testing")
+>>>>>>> a4373aa ([Update] - Added new file)
