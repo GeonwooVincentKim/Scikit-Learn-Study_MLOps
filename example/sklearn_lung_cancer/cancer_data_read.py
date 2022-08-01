f1a13580db46b60b64eeb368093704c84faebfaf
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-01 17:19:07 +0900

    [Update] - Updated files

---
+++ example/sklearn_lung_cancer/cancer_data_read.py
@@ -0,0 +1,5 @@
+import pandas as pd
+# dataset = pd.read_csv(r"/mnt/e/MLOps/new_sklearn/Scikit-Learn-Study_MLOps/example/sklearn_lung_cancer/ThoraricSurgery.csv")
+dataset = pd.read_csv("../public/csv/ThoraricSurgery.csv")
+
+print('Dataset -> {0}'.format(dataset))
