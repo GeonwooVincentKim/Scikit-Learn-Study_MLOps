0246cb2c34369a814ef7e477611d9c7028e7f2e4
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-02 16:12:31 +0900

    [Feature] - Changed File name and print data-read result

--- example/sklearn_lung_cancer/cancer_data_read.py
+++ example/sklearn_lung_cancer/cancer_data_read.py
@@ -1,5 +1,8 @@
+import sys
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
 
 # dataset = pd.read_csv(r"/mnt/e/MLOps/new_sklearn/Scikit-Learn-Study_MLOps/example/sklearn_lung_cancer/ThoraricSurgery.csv")
 # Import csv files from `public/csv/` file-directory
@@ -16,3 +19,14 @@ train, test = train_test_split(dataset)
 
 # The predicted column is "293" which is
 train_x = train.drop(["293"], axis=1)
+test_x = test.drop(["293"], axis=1)
+train_y = train[["293"]]
+test_y = test[["293"]]
+
+alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+lr.fit(train_x, train_y)
+
+predicted_qualities = lr.predict(test_x)
